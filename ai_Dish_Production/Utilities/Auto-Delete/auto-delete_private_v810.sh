30c714c1752a4cef40d8e5cd57aa65f9a7d9b2b7
mgretzing <mgretzing@EAT-FBSY063.ad.spirentcom.com>
2021-12-13 08:55:14 -0600

    removed "extra" / .. maybe?

--- ai_Dish_Production/Utilities/Auto-Delete/auto-delete_private_v810.sh
+++ ai_Dish_Production/Utilities/Auto-Delete/auto-delete_private_v810.sh
@@ -6,7 +6,7 @@ PASSWORD=$4
 DATE=`date  --date="$TARGETDATE days ago" +%s%N | cut -b1-13`
 
 #TOKEN=`curl -k -s -u "$USERNAME:$PASSWORD" https://$HOSTNAME/velocity/api/auth/v2/token | jq '.token'`
-TOKEN=`curl -k -s -u "$USERNAME:$PASSWORD" https://$HOSTNAME/velocity/api/auth/v2/token | grep -o '"token":"[^"]*' | cut -d'"' -f4`
+TOKEN=`curl -k -s -u "$USERNAME:$PASSWORD" https:/$HOSTNAME/velocity/api/auth/v2/token | grep -o '"token":"[^"]*' | cut -d'"' -f4`
 
 echo "curl -k -s -X PUT -H \"X-Auth-Token:$TOKEN\" \"https://$HOSTNAME/ito/reporting/v1/autodelete?olderThan=$DATE\""
-curl -k -s -X PUT -H "X-Auth-Token:$TOKEN" "https://$HOSTNAME/ito/reporting/v1/autodelete?olderThan=$DATE"
+curl -k -s -X PUT -H "X-Auth-Token:$TOKEN" "https:/$HOSTNAME/ito/reporting/v1/autodelete?olderThan=$DATE"
