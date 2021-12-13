33f03f84e18828e644dcfdd915089486a3b19f29
mgretzing <mgretzing@EAT-FBSY063.ad.spirentcom.com>
2021-12-13 10:26:22 -0600

    another fix

--- ai_Dish_Production/Utilities/Auto-Delete/auto-delete_private_v810.sh
+++ ai_Dish_Production/Utilities/Auto-Delete/auto-delete_private_v810.sh
@@ -5,7 +5,6 @@ USERNAME=$3
 PASSWORD=$4
 DATE=`date  --date="$TARGETDATE days ago" +%s%N | cut -b1-13`
 
-#TOKEN=`curl -k -s -u "$USERNAME:$PASSWORD" https://$HOSTNAME/velocity/api/auth/v2/token | jq '.token'`
 TOKEN=`curl -k -s -u "$USERNAME:$PASSWORD" https:/$HOSTNAME/velocity/api/auth/v2/token | grep -o '"token":"[^"]*' | cut -d'"' -f4`
 
 echo "curl -k -s -X PUT -H \"X-Auth-Token:$TOKEN\" \"https://$HOSTNAME/ito/reporting/v1/autodelete?olderThan=$DATE\""
